419f727b666c399ca91cdfbd4c4003c719486f29
Дмитриев Евгений Дмитриевич <dmitrievedm@isc.confident.spb.ru>
2021-04-01 11:59:15 +0300

    Add_PostAdmin

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+# из файла models импортируем модель Post
+from .models import Post
 
-# Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    # перечисляем поля, которые должны отображаться в админке
+    list_display = ("text", "pub_date", "author") 
+    # добавляем интерфейс для поиска по тексту постов
+    search_fields = ("text",) 
+    # добавляем возможность фильтрации по дате
+    list_filter = ("pub_date",) 
+
+    
+admin.site.register(Post, PostAdmin) 
