5eb0c147757814019ddf1199e946b4f9f0ead940
Zapadniy-lgtm <zapadniy@bk.ru>
2021-04-11 21:06:34 +0300

    11042021

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -21,3 +21,13 @@ def index_ok(request):
 def index_ok_too(request):
     latest = Post.objects.order_by("-pub_date")[:11]
     return render(request, "index.html", {"posts": latest}) 
+
+
+    #def index(request):
+# одна строка вместо тысячи слов на SQL
+    #latest = Post.objects.order_by('-pub_date')[:10]
+    # собираем тексты постов в один, разделяя новой строкой
+    #output = []
+    #for item in latest:
+        #output.append(item.text)
+    #return HttpResponse('\n'.join(output)) 
